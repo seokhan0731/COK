c92a547c8b013b68982ff2e88d76bee33eb95619
ChoInHeum <heumi711@gmail.com>
2026-05-09 18:48:50 +0900

    init

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+    return (
+        <>
+            <text>Home Page</text>
+        </>
+    )
+}
+
+export default HomePage;
---
+++ frontend/src/pages/TestPage.tsx
@@ -0,0 +1,11 @@
+const TestPage = () => {
+  return (
+    <>
+      <div className="flex flex-1 justify-center-safe">
+        <text className="text-4xl font-black">TestPage </text>
+      </div>
+    </>
+  )
+}
+
+export default TestPage
