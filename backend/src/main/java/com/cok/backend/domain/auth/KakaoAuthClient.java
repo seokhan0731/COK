3022b33002c5e310e7516d7c4491cf076a6a2779
seokhan0731 <wistin@naver.com>
2026-05-30 02:14:28 +0900

    feat: 카카오 토큰 발급을 위한 FeignClient 및 응답 DTO 추가

---
+++ backend/src/main/java/com/cok/backend/domain/auth/KakaoAuthClient.java
@@ -0,0 +1,18 @@
+package com.cok.backend.domain.auth;
+
+import com.cok.backend.domain.auth.dto.KakaoTokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "kakaoClient", url = "https://kauth.kakao.com/oauth")
+public interface KakaoAuthClient {
+
+    @PostMapping(value = "/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    KakaoTokenResponse getAccessToken(@RequestParam("grant_type") String grantType,
+                                      @RequestParam("client_id") String apiKey,
+                                      @RequestParam("redirect_uri") String uri,
+                                      @RequestParam("code") String code,
+                                      @RequestParam("client_secret") String secretCode);
+}
