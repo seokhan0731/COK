d17b57625b0444c9365ceccb8e17100c28d54567
seokhan0731 <wistin@naver.com>
2026-06-04 02:12:53 +0900

    refactor: 코드 가독성을 위한 메소드 분리

--- backend/src/main/java/com/cok/backend/domain/hub/HubService.java
+++ backend/src/main/java/com/cok/backend/domain/hub/HubService.java
@@ -24,35 +24,71 @@ public class HubService {
     private final MasterCertificationRepository masterCertificationRepository;
     private final TagMappingRepository mappingRepository;
 
+    /**
+     * 페이지에 노출할 아이템 조회 및 dto(HubResponse) 조립
+     * 잘못된 파라미터인 경우, 빈 리스트를 반환
+     * 전체 조회를 요청하는 경우, 직무->자격증 순으로 구성
+     * 배열이나, 리스트 대신 맵을 사용 -> 불필요한 분기 처리 필요 X
+     *
+     * @param type 요청 파라미터
+     * @return 요청 파라미터에 따른 HubResponse
+     */
     public HubResponse getHubItems(String type) {
         List<HubItem> items = new ArrayList<>();
 
-        if (type == null || type.equals("job")) {
+        if (isJobOrAll(type)) {
             List<TagMapping> allColumns = mappingRepository.findAllWithTag();
             Map<Long, List<String>> jobTags = new HashMap<>();
 
-            for (TagMapping column : allColumns) {
-                Long jobId = column.getMasterJob().getId();
-                String tagName = column.getWantedTag().getName();
-
-                if (!jobTags.containsKey(jobId)) {
-                    jobTags.put(jobId, new ArrayList<>());
-                }
-
-                jobTags.get(jobId).add(tagName);
-            }
-
-            masterJobRepository.findAll().forEach(job -> {
-                List<String> subItems = jobTags.getOrDefault(job.getId(), new ArrayList<>());
-                items.add(HubItem.fromJob(job, subItems));
-            });
+            fillMap(allColumns, jobTags);
+            addJobItemsByMap(items, jobTags);
         }
 
-        if (type == null || type.equals("certification")) {
-            masterCertificationRepository.findAll().forEach(certification -> {
-                items.add(HubItem.fromCertification(certification));
-            });
+        if (isCertificationOrAll(type)) {
+            addCertificationItems(items);
         }
         return new HubResponse(items);
+    }
+
+    /**
+     * 마스터 직무와 이에 해당하는 원티드 세부 직군들 매핑
+     * 마스터 직무 id를 key, 세부 직군들이 value로 구성
+     *
+     * @param columns 마스터 직무, 원티드 룩업, 원티드 태그 테이블이 결합된 객체
+     * @param map 마스터 직무를: [원티드 직무] 형태의 완성된 map
+     */
+    private void fillMap(List<TagMapping> columns, Map<Long, List<String>> map) {
+        for (TagMapping column : columns) {
+            Long jobId = column.getMasterJob().getId();
+            String tagName = column.getWantedTag().getName();
+
+            if (!map.containsKey(jobId)) {
+                map.put(jobId, new ArrayList<>());
+            }
+
+            map.get(jobId).add(tagName);
+        }
+    }
+
+    private void addJobItemsByMap(List<HubItem> items, Map<Long, List<String>> map) {
+        masterJobRepository.findAll().forEach(job -> {
+            //매핑되는 세부 직무가 없는 경우, 빈 리스트를 반환하기 위해, getOrDefault 사용
+            List<String> subItems = map.getOrDefault(job.getId(), new ArrayList<>());
+            items.add(HubItem.fromJob(job, subItems));
+        });
+    }
+
+    private void addCertificationItems(List<HubItem> items) {
+        masterCertificationRepository.findAll().forEach(certification -> {
+            items.add(HubItem.fromCertification(certification));
+        });
+    }
+
+    private boolean isJobOrAll(String type) {
+        return type == null || type.equals("job");
+    }
+
+    private boolean isCertificationOrAll(String type) {
+        return type == null || type.equals("certification");
     }
 }
