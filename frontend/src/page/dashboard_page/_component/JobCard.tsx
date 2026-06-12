e6e012dc069e136a7ce3c3e288785a6440224146
ojuno <ojume223@naver.com>
2026-06-12 18:37:44 +0900

    fix: 오타 수정

--- frontend/src/page/dashboard_page/_component/JobCard.tsx
+++ frontend/src/page/dashboard_page/_component/JobCard.tsx
@@ -1,7 +1,7 @@
 /* src/page/dashboard_page/_component/JobCard.tsx */
 
 import clsx from 'clsx';
-import { JOB_META, type JobType } from '../../../type/DashboardType';
+import { JOB_META, type JobType } from '../../../type/dashboardType';
 
 type JobCardProps = {
   jobId: JobType;
